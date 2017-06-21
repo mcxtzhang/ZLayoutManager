975c2b6756164311666b712363d44d363dae3c8e
zhangxutong <zhangxutong@imcoming.cn>
2017-06-21 18:06:47 +0800

    【探探选择头像Demo】

--- app/src/main/java/com/mcxtzhang/flowlayoutmanager/avatar/TanTanAvatarActivity.java
+++ app/src/main/java/com/mcxtzhang/flowlayoutmanager/avatar/TanTanAvatarActivity.java
@@ -3,6 +3,7 @@ package com.mcxtzhang.flowlayoutmanager.avatar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
@@ -12,6 +13,7 @@ import com.mcxtzhang.flowlayoutmanager.R;
 import com.mcxtzhang.flowlayoutmanager.swipecard.SwipeCardBean;
 import com.squareup.picasso.Picasso;
 
+import java.util.Collections;
 import java.util.List;
 
 public class TanTanAvatarActivity extends AppCompatActivity {
@@ -39,5 +41,37 @@ public class TanTanAvatarActivity extends AppCompatActivity {
                 Picasso.with(TanTanAvatarActivity.this).load(swipeCardBean.getUrl()).into((ImageView) viewHolder.getView(R.id.iv));
             }
         });
+
+        ItemTouchHelper.Callback callback = new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                return makeMovementFlags(ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT, 0);
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                int fromPosition = viewHolder.getAdapterPosition();//得到拖动ViewHolder的position
+                int toPosition = target.getAdapterPosition();//得到目标ViewHolder的position
+                if (fromPosition < toPosition) {
+                    for (int i = fromPosition; i < toPosition; i++) {
+                        Collections.swap(mDatas, i, i + 1);
+                    }
+                } else {
+                    for (int i = fromPosition; i > toPosition; i--) {
+                        Collections.swap(mDatas, i, i - 1);
+                    }
+                }
+                mAdapter.notifyItemMoved(fromPosition, toPosition);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(mRv);
     }
 }
